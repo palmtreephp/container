b281d7dcab77e2f465fe16930ec2ecb5b4cd93a9
andyexeter <palmer.andy@gmail.com>
2019-02-02 10:21:35 +0000

    Improve absolute path checking. No need for regex

--- src/ContainerFactory.php
+++ src/ContainerFactory.php
@@ -43,7 +43,7 @@ class ContainerFactory
     private function parseImports(array $data, string $dir): array
     {
         foreach ($data['imports'] ?? [] as $key => $import) {
-            $resource = self::getImportResource($dir, $import);
+            $resource = $this->getImportResource($dir, $import);
 
             $extension = pathinfo($resource, PATHINFO_EXTENSION);
 
@@ -64,15 +64,30 @@ class ContainerFactory
         require $file;
     }
 
-    private static function getImportResource(string $dir, array $import): string
+    private function getImportResource(string $dir, array $import): string
     {
         $resource = $import['resource'];
 
         // Prefix the directory if resource is not an absolute path
-        if ($resource[0] !== DIRECTORY_SEPARATOR && !preg_match('~\A[A-Z]:(?![^/\\\\])~i', $resource)) {
+        if (!$this->isAbsolutePath($resource)) {
             $resource = "$dir/$resource";
         }
 
         return $resource;
+    }
+
+    private function isAbsolutePath(string $file): bool
+    {
+        if ($file[0] === '/' || $file[0] === '\\'
+            || (\strlen($file) > 3 && \ctype_alpha($file[0])
+                && $file[1] === ':'
+                && ($file[2] === '\\' || $file[2] === '/')
+            )
+            || null !== \parse_url($file, PHP_URL_SCHEME)
+        ) {
+            return true;
+        }
+
+        return false;
     }
 }
